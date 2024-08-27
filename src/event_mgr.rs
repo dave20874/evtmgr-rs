b05958af177d50c8626fc549960793356aee58dc
David Wheeler <dwheeler20874@gmail.com>
2024-08-26 23:16:31 -0400

    Still struggling to get something buildable.

--- src/event_mgr.rs
+++ src/event_mgr.rs
@@ -5,6 +5,7 @@
 // TODO : Document how event handlers register callbacks for events
 
 use std::{collections::VecDeque, fmt::Debug, sync::{Arc, Mutex}};
+use lazy_static::lazy_static;
 
 // TODO-DW : Eliminate 'static lifetime on channels managed by EventMgr.
 
@@ -12,16 +13,14 @@ use std::{collections::VecDeque, fmt::Debug, sync::{Arc, Mutex}};
 // The singleton manager of events manages the event queue.
 pub struct EventMgr
 {
-    event_queue: VecDeque<&'static dyn EventChannelIf>,
+    event_queue: Mutex<VecDeque<&'static dyn EventChannelIf>>,
 }
 
-
-
-pub trait EventChannelIf: Sync {
+pub trait EventChannelIf: Sync+Send {
     fn service_event(&self);
 }
 
-pub trait EventHandler<T> {
+pub trait EventHandler<T>: Sync+Send {
     fn on_event(&self, event: &T);
 }
 
@@ -30,13 +29,14 @@ impl EventMgr
     // private function to create the singleton event manager in lazy_static, above.
     fn new() -> EventMgr {
         EventMgr {
-            event_queue: VecDeque::new(),
+            event_queue: Mutex::new(VecDeque::new()),
         }
     }
 
-    fn queue(&self, callback: &dyn EventChannelIf)
+    fn queue(&self, callback: &'static dyn EventChannelIf)
     {
-        self.event_queue.push_back(callback);
+        let mut event_queue = self.event_queue.lock().unwrap();
+        event_queue.push_back(callback);
     }
 
     pub fn poll()
@@ -44,12 +44,12 @@ impl EventMgr
         let mut poll_again = true;
         while poll_again {
             let chan_opt: Option<&dyn EventChannelIf> = {
-                let mut mgr = EVENT_MGR.lock().unwrap();
-                if mgr.event_queue.is_empty() {
+                let mut event_queue = EVENT_MGR.event_queue.lock().unwrap();
+                if event_queue.is_empty() {
                     None
                 }
                 else {
-                    let channel = mgr.event_queue.pop_front().unwrap();
+                    let channel = event_queue.pop_front().unwrap();
                     Some(channel)
                 }
             };
@@ -66,7 +66,9 @@ impl EventMgr
     }
 }
 
-static EVENT_MGR: Mutex<EventMgr> = Mutex::new(EventMgr::new());
+lazy_static! {
+    static ref EVENT_MGR: EventMgr = EventMgr::new();
+}
 
 
 
@@ -77,23 +79,24 @@ static EVENT_MGR: Mutex<EventMgr> = Mutex::new(EventMgr::new());
 // TODO: Figure out how to declare F as &dyn with a lifetime.
 
 pub struct EventChannel<'a, T>
-where T: Debug
+where T: Debug+Sync
 {
-    handlers: Vec<Mutex<&'a dyn EventHandler<T>>>,
-    event_queue: Arc<Mutex<VecDeque<T>>>,
+    handlers: Mutex<Vec<&'a dyn EventHandler<T>>>,
+    event_queue: Mutex<VecDeque<T>>,
 }
 
 impl<'a, T> EventChannel<'a, T>
-where T: Debug
+where T: Debug+Sync+Send
 {
     pub fn new() -> EventChannel<'a, T>
     {
-        EventChannel { handlers: Vec::new(), event_queue: Arc::new(Mutex::new(VecDeque::new())) }
+        EventChannel { handlers: Mutex::new(Vec::new()), event_queue: Mutex::new(VecDeque::new()) }
     }
 
-    pub fn subscribe(&mut self, handler: Mutex<&'a dyn EventHandler<T>>) 
+    pub fn subscribe(&self, handler: &'a dyn EventHandler<T>) 
     {
-        self.handlers.push(handler);
+        let mut handler_list = self.handlers.lock().unwrap();
+        handler_list.push(handler);
         println!("Subscribe added handler.");
     }
 
@@ -106,22 +109,22 @@ where T: Debug
         event_queue.push_back(e);
 
         // tell manager to call us back.
-        let mgr = EVENT_MGR.unlock.unwrap();
-        mgr.unlock().queue(self);
+        EVENT_MGR.queue(self);
     }
 
 }
 
 impl<'a, T> EventChannelIf for EventChannel<'a, T>
-where T: Debug
+where T: Debug+Sync+Send
 {
     fn service_event(&self) 
     {
         let mut event_queue = self.event_queue.lock().unwrap();
         let data = event_queue.pop_front().unwrap();
-        match &self.handler {
-            Some(handler) => { handler.on_event(&data); }
-            None => { println!("No handler."); }
+
+        let handlers = self.handlers.lock().unwrap();
+        for handler in handlers.iter() {
+            handler.on_event(&data);
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -9,21 +9,21 @@ struct EventData {
 
 struct System<'a> {
     ch1: EventChannel<'a, EventData>,
-    handler1: Mutex<&'a dyn &EventData>,  // Fixme.
+    // handler1: Mutex<&'a dyn &EventData>,  // Fixme.
 }
 
 impl<'a> System<'a>
 {
     pub fn new() -> Self
     {
-        let mut ch1 = EventChannel::<EventData, &dyn Fn(&EventData)>::new();
+        let mut ch1 = EventChannel::<EventData>::new();
 
         let mut sys = System {
             ch1: ch1,
         };
 
         // Register a listener
-        sys.ch1.subscribe(&self );
+        sys.ch1.subscribe(&sys);
 
         sys
     }
@@ -32,7 +32,7 @@ impl<'a> System<'a>
     {
         // Publish a message
         // let e = Box::new(EventData {data1: 69});
-        self.ch1.publish(&EventData {data1: 69});
+        self.ch1.publish(EventData {data1: 69});
     }
 
     pub fn poll(&self)
@@ -45,7 +45,7 @@ impl<'a> System<'a>
 
 impl<'a> EventHandler<EventData> for System<'a> 
 {
-    fn on_event(&self, data: &T) 
+    fn on_event(&self, data: &EventData) 
     {
         println!("in System on_event()");
     }
